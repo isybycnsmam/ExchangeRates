32d9373dd76d41e1eab88fae5896d9e82abc1995
Maciej Scholz <maciej-scholz@tlen.pl>
2020-11-14 19:24:49 +0100

    Added constructor that calculates exchange rate

--- ExchangeRates/DTOs/CurrencyExchange.cs
+++ ExchangeRates/DTOs/CurrencyExchange.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using ExchangeRates.Models;
 
 namespace ExchangeRates.DTOs
 {
     public sealed class CurrencyExchange
     {
+        public CurrencyExchange(
+            EuroExchange from,
+            EuroExchange to,
+            DateTime date)
+        {
+            CurrencyFrom = from.Currency;
+            CurrencyTo = to.Currency;
+            Date = date;
+            ExchangeRate = Math.Round(1 / from.ExchangeRate * to.ExchangeRate, 4);
+        }
+
         public DateTime Date { get; set; }
         public string CurrencyFrom { get; set; }
         public string CurrencyTo { get; set; }
