992476027bc1a3c2e12ed548176a592628699036
Maciej Scholz <maciej-scholz@tlen.pl>
2020-11-15 15:05:43 +0100

    Added comments

--- ExchangeRates/Models/BankingHoliday.cs
+++ ExchangeRates/Models/BankingHoliday.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ExchangeRates.Models
 {
+    /// <summary>
+    /// Db model for storing dates that are banking holidays
+    /// </summary>
     public class BankingHoliday
     {
         public BankingHoliday(DateTime date)
